414662406f0b2e237ee62b93c16cf78ec6b05686
ahmose231 <36313066+ahmose231@users.noreply.github.com>
2022-01-09 22:02:54 +0300

    Create 10b.c

---
+++ 10b.c
@@ -0,0 +1,102 @@
+//incomplete
+
+#include <stdio.h>
+#include <string.h>
+
+#define LEN 128
+
+int zing(int c);
+
+void main()
+{
+	char line[LEN];
+	int len;
+	char stack[LEN];
+	int si;
+	int flag;
+	int total=0;
+	int bigtotal[100];
+	int bigtotalindex=0;
+	while(fgets(line,LEN,stdin)!=NULL)
+	{
+	    total=0;
+		flag=0;
+		si=0;
+		len=strlen(line);
+		if(len<2) continue;
+		line[len-1]='\0';
+		for(int i=0;i<len;i++)
+		{
+			if(strchr("{<[(", line[i])!=NULL)
+				stack[si++]=line[i];
+			else
+			{
+				if(line[i]==')')
+				{
+					if(stack[si-1]=='(')
+						si--;
+					else
+						flag=1;
+				}
+				else 
+				{
+					if(stack[si-1]==line[i]-2)
+						si--;
+					else
+						flag=1;
+				}
+			}
+			
+			if(flag)
+			{
+//				total += zing(line[i]);	
+				break;
+			}
+		}
+		
+		if(!flag)
+		{
+		    for(int i=si-1;i>=0;i--)
+		    {
+		        total *= 5;
+		        total += bling(stack[i]);
+		    }
+		}
+		
+		bigtotal[bigtotalindex++]=total;
+	}
+					
+	
+	for(int i=0;i<bigtotalindex;i++)
+	    printf("%d\n",bigtotal[i]);
+	return;
+}
+
+
+int zing(int c)
+{
+	switch(c)
+	{	
+		case ')': return 3;
+		case ']': return 57;
+		case '}': return 1197;
+		case '>': return 25137;
+	}
+	
+	return 0;
+}
+
+
+int bling(int c)
+{
+    switch(c)
+    {
+		case '(': return 1;
+		case '[': return 2;
+		case '{': return 3;
+		case '<': return 4;
+	}
+	
+	return;
+}
+
