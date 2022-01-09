7f5dbae8f9d323f201e152f665bf297423d5a007
ahmose231 <36313066+ahmose231@users.noreply.github.com>
2022-01-09 16:11:39 +0300

    Update 15.c

--- 15.c
+++ 15.c
@@ -8,54 +8,46 @@
 void move(int x, int y, int pile, int fromx, int fromy);
 
 int m[SQUARESIZE][SQUARESIZE];
-unsigned int bestscore=0-1;
+unsigned int bestscore = 0 - 1;
 
-void main()
-{
-    char line[LINESIZE];
-    int k=0;
-    while(fgets(line,LINESIZE,stdin)!=NULL)
-    {
-        if(strlen(line)<2) continue;
-        for(int i=0;i<SQUARESIZE;i++)
-            m[k][i]=line[i]-'0';
-        k++;
-    }
-   
-    
-    move(0,0,0,-1,-1);
-    
-    return;
+void main() {
+        char line[LINESIZE];
+        int k = 0;
+        while (fgets(line, LINESIZE, stdin) != NULL) {
+                if (strlen(line) < 2) continue;
+                for (int i = 0; i < SQUARESIZE; i++)
+                        m[k][i] = line[i] - '0';
+                k++;
+        }
+
+        move(0, 0, 0, -1, -1);
+
+        return;
 }
 
-
-
-void move(int x, int y, int pile, int fromx, int fromy)
-{   
-    if(!(x==0&&y==0))
-    	pile += m[x][y];
-    if(x==SQUARESIZE-1 && y==SQUARESIZE-1)
-    {
-        if(pile<bestscore)
-        {
-            printf("new best score: %d\n",pile);
-            bestscore=pile;
+void move(int x, int y, int pile, int fromx, int fromy) {
+        if (!(x == 0 && y == 0))
+                pile += m[x][y];
+        if (x == SQUARESIZE - 1 && y == SQUARESIZE - 1) {
+                if (pile < bestscore) {
+                        printf("new best score: %d\n", pile);
+                        bestscore = pile;
+                }
+                return;
         }
-        return;
-    }
 
- /* if(x>0 && fromx!=x-1)
-        move(x-1, y, pile, x, y);    */     
-    
-    if(x<SQUARESIZE-1 && fromx!=x+1)
-        move(x+1, y, pile, x, y);
-   
- /* if(y>0 && fromy!=y-1)
-        move(x, y-1, pile, x, y);     */ 
-    
-    if(y<SQUARESIZE-1 && fromy!=y+1)
-        move(x, y+1, pile, x, y);
-    
-    return;
-    
+        /* if(x>0 && fromx!=x-1)
+               move(x-1, y, pile, x, y);    */
+
+        if (x < SQUARESIZE - 1 && fromx != x + 1)
+                move(x + 1, y, pile, x, y);
+
+        /* if(y>0 && fromy!=y-1)
+               move(x, y-1, pile, x, y);     */
+
+        if (y < SQUARESIZE - 1 && fromy != y + 1)
+                move(x, y + 1, pile, x, y);
+
+        return;
+
 }
